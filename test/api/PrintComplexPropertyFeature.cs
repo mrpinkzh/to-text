62e97ca5fa705dc7f82dd6245d1c1e46d42416ce
mrpinkzh <mrpinkzh@gmail.com>
2014-07-31 17:06:46 +0200

    Added easiest complex property test.

---
+++ test/api/PrintComplexPropertyFeature.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using ToText.Infrastructure;
+
+namespace ToText.Api
+{
+    [TestFixture]
+    public class PrintComplexPropertyFeature
+    {
+        [Test]
+        public void OnDojo_WithMaster_ShouldReturnDojosMaster()
+        {
+            var dojo = new Dojo {Master = new Ninja {Name = "Miyagi"}};
+            string text = dojo.ToText(d => d.Master);
+            Assert.That(text, Is.EqualTo("Dojo: Master = 'ToText.Infrastructure.Ninja'"));
+        }
+    }
+}
---
+++ test/core/Infrastructure/Dojo.cs
@@ -0,0 +1,13 @@
+﻿namespace ToText.Infrastructure
+{
+    public class Dojo
+    {
+        private Ninja master;
+
+        public Ninja Master
+        {
+            get { return master; }
+            set { master = value; }
+        }
+    }
+}
