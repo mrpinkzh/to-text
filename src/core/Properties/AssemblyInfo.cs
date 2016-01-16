e09992c3e6832650d897e3d6a032b9a7a7bfd99f
mrpinkzh <mrpinkzh@gmail.com>
2016-01-16 10:20:28 +0100

    removed nuspec.. using csproj generation now.

--- src/core/Properties/AssemblyInfo.cs
+++ src/core/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ToText.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("see: https://github.com/mrpinkzh/to-text")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aias Engineering")]
 [assembly: AssemblyProduct("ToText")]
